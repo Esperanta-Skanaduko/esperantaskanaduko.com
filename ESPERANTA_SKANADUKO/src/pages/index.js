df92314c935b397285549b65a6b1273ae2a80eb4
Victor Williams <vrwilliams719@gmail.com>
2021-03-15 19:24:51 -0400

    i dont know

--- ESPERANTA_SKANADUKO/src/pages/index.js
+++ ESPERANTA_SKANADUKO/src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
-// import { Link } from "gatsby"
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 import LandingPage from './landingPage'
 
 // let contents;
@@ -30,7 +30,24 @@ import LandingPage from './landingPage'
 
 const IndexPage = () => (
   <>
-  <LandingPage></LandingPage>
+  <Layout>
+//     <SEO title="Home" />
+//     <br/>
+//     <h1>Eldona grupo por literaturo en Esperanto</h1>
+//     {/* <StaticImage
+//       src="../images/gatsby-astronaut.png"
+//       width={300}
+//       quality={95}
+//       formats={["AUTO", "WEBP", "AVIF"]}
+//       alt="A Gatsby astronaut"
+//       style={{ marginBottom: `1.45rem` }}
+//     /> */}
+//     <p>
+//       <Link to="/LandingPage/">Landing page</Link> <br />
+//       <Link to="/using-typescript/">Go to "Using TypeScript"</Link><br />
+//       <a href="https://mangadex.org/group/18541/esperanta-skanaduko/" target="_blank" rel="noreferrer">MangaDex</a><br />
+//     </p>
+//   </Layout>
   </>
 )
 
