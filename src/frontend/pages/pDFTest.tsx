75e76bee50c294d8de9917bdaf2e2b90d1ec1fd0
Victor Williams <vrwilliams719@gmail.com>
2023-11-21 16:05:43 -0500

    update pdftest

--- src/frontend/pages/pDFTest.tsx
+++ src/frontend/pages/pDFTest.tsx
@@ -1,31 +1,52 @@
 import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
+// Set the worker source URL
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const PDFTest = () => {
-  const [pageNumber] = useState<number>(1);
+  const [pageNumber, setPageNumber] = useState<number>(1);
   const [numPages, setNumPages] = useState<number>(0);
-
-  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.js',
-    import.meta.url,
-  ).toString();
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
   };
+
+  const handleNextPage = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
   return (
     <div>
-      <p>PDFTest</p>
+      <button onClick={handlePrevPage} disabled={pageNumber <= 1}>
+          Previous Page
+        </button>
+        <button onClick={handleNextPage} disabled={pageNumber >= numPages}>
+          Next Page
+        </button>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
       <Document
         file="https://arxiv.org/pdf/quant-ph/0410100.pdf"
         onLoadSuccess={onDocumentLoadSuccess}
       >
         <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
         <p>
           Page {pageNumber} of {numPages}
         </p>
-      </Document>
+
+      </div>
     </div>
   );
 };
