1c92598878aa2c4774394f177bd74856fc5d7ccc
Victor Williams <vrwilliams719@gmail.com>
2024-09-04 16:55:00 -0400

    Update AboutPage content with website information and license details

--- src/frontend/pages/about/aboutPage.tsx
+++ src/frontend/pages/about/aboutPage.tsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 const AboutPage = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h2>About the website</h2>
+      <p>Welcome to libraro, a new online Esperanto library. This project’s goal is to provide a well-organized library of original and translated texts</p>
+      <h2>License issues</h2>
+      <p> Many texts are old enough to automatically be in the public domain, many are in the public domain in some countries while copyrighted in others. This website publishes works for which the international minimum of 50 years after the author’s death is satisfied. It is the reader's responsibility to know his or her country’s copyright laws. </p>
     </div>
   );
 };
